a1895f658841b7ba0dda32ddc3f5819564bdc8a4
Nadeem <nadeemasgar18@gmail.com>
2022-01-10 02:00:08 +0530

    failure and success page created & route added

--- 5. Node.js (5 Jan 2022)/API/Newsletter-signUp/app.js
+++ 5. Node.js (5 Jan 2022)/API/Newsletter-signUp/app.js
@@ -33,7 +33,7 @@ app.post("/", function (req, res) {
 
   const jsonData = JSON.stringify(data);
 
-  const url = "https://us20.api.mailchimp.com/3.0/lists/c72da071b9/members";
+  const url = "https://us20.api.mailchimp.com/3.0/lists/c72da071b9";
 
   const options = {
     method: "POST",
@@ -41,6 +41,14 @@ app.post("/", function (req, res) {
   };
 
   const request = https.request(url, options, function (response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    }
+    else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
     response.on("data", function (data) {
       console.log(JSON.parse(data));
     });
@@ -50,10 +58,13 @@ app.post("/", function (req, res) {
   request.end();
 });
 
+app.post('/failure', function (req, res) {
+  res.redirect("/");
+})
+
 app.listen(4000, function () {
   console.log("Listnening on 4000");
 });
 
 // 61c753130edaed7138ac71a19ed3befd-us20
-
 // c72da071b9
