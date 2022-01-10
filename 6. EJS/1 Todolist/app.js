7f586a8f4330b7a48f7f1ea4bbf2c786915c7ab7
Nadeem <nadeemasgar18@gmail.com>
2022-01-10 17:12:48 +0530

    Added Layout using EJS for resuability of code

--- 6. EJS/1 Todolist/app.js
+++ 6. EJS/1 Todolist/app.js
@@ -53,6 +53,10 @@ app.post("/work", function (req, res) {
     let item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work");
+});
+
+app.get("/about", function (req, res) {
+    res.render("about");
 })
 
 app.listen(3000, function () {
