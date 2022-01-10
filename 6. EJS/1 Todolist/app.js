6813c7ff55a0f16532a7fbcdbac4299ccaa65ee3
Nadeem <nadeemasgar18@gmail.com>
2022-01-10 12:28:20 +0530

    weekend weekday using EJS

--- 6. EJS/1 Todolist/app.js
+++ 6. EJS/1 Todolist/app.js
@@ -5,18 +5,28 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs"); // To tell browser that we are using ejs
+
+
+
 app.get("/", (req, res) => {
-  //   res.send("Hello");  // response from server to browser ... help to perform logic and send back to browser
-  //   res.send("<h1> Hello.. today is Monday </h1>"); // sending HTML to browser .. send is is used for final sending instruction
 
-  /*   res.write("<h1> Hello.. today is Monday </h1>"); // res.write() to send multiple pieces of data
-       res.write("<h2> This is Nadeem </h2>");
-       res.send();
-  */
+    var today = new Date();
+    var currentDay = today.getDate();
+    var day = "";
 
-       res.sendFile(__dirname + "/index.html");  // To send entire html file to browser
+    if(currentDay === 6 || currentDay === 0) {
+        day = "Weekend";
+    }
+    else {
+        day = "Weekday";
+    }
+
+    res.render("list", {kindOfDay : day});
 });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+
+
