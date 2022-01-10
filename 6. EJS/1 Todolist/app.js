b2291ae1fd7e567e11abac6bf33cc7f398d465cb
Nadeem <nadeemasgar18@gmail.com>
2022-01-10 11:38:06 +0530

    res.sendFile()

--- 6. EJS/1 Todolist/app.js
+++ 6. EJS/1 Todolist/app.js
@@ -9,9 +9,12 @@ app.get("/", (req, res) => {
   //   res.send("Hello");  // response from server to browser ... help to perform logic and send back to browser
   //   res.send("<h1> Hello.. today is Monday </h1>"); // sending HTML to browser .. send is is used for final sending instruction
 
-  res.write("<h1> Hello.. today is Monday </h1>"); // res.write() to send multiple pieces of data
-  res.write("<h2> This is Nadeem </h2>");
-  res.send();
+  /*   res.write("<h1> Hello.. today is Monday </h1>"); // res.write() to send multiple pieces of data
+       res.write("<h2> This is Nadeem </h2>");
+       res.send();
+  */
+
+       res.sendFile(__dirname + "/index.html");  // To send entire html file to browser
 });
 
 app.listen(3000, function () {
