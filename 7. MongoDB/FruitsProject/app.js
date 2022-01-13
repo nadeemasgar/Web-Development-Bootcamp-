51012e7dde32986400d6a9e8675ca450ddf5b9a2
Nadeem <nadeemasgar18@gmail.com>
2022-01-13 16:18:57 +0530

    Multiple collections inserted in DB

--- 7. MongoDB/FruitsProject/app.js
+++ 7. MongoDB/FruitsProject/app.js
@@ -24,3 +24,17 @@ const fruit = new Fruit ({
 });
 
 // fruit.save();
+
+const personSchema = new mongoose.Schema ( {
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "John", 
+    age: 37
+});
+
+person.save();
