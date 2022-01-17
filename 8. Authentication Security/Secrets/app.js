e12d2e68ea94334bb37f4ce6bab43e83e71d9856
Nadeem <nadeemasgar18@gmail.com>
2022-01-17 23:37:12 +0530

    Add Environment vars

--- 8. Authentication Security/Secrets/app.js
+++ 8. Authentication Security/Secrets/app.js
@@ -1,50 +1,52 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption"); 
-
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
+console.log(process.env.SECRET);
+
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.set("view engine", "ejs"); // For EJS template
-app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema ({
-    email : String,
-    password : String
+  email: String,
+  password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret : secret, encryptedFields : ["password"] });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema);
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res){
   res.render("home");
 });
 
-app.get("/login", function (req, res) {
+app.get("/login", function(req, res){
   res.render("login");
 });
 
-app.get("/register", function (req, res) {
+app.get("/register", function(req, res){
   res.render("register");
 });
 
-app.post("/register", function (req, res) {
-  const newUser = new User({
+app.post("/register", function(req, res){
+  const newUser =  new User({
     email: req.body.username,
-    password: req.body.password,
+    password: req.body.password
   });
-
-  newUser.save(function (err) {
+  newUser.save(function(err){
     if (err) {
       console.log(err);
     } else {
@@ -53,24 +55,29 @@ app.post("/register", function (req, res) {
   });
 });
 
-app.post("/login", function (req, res) {
+app.post("/login", function(req, res){
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne(
-    { email: username, password: password },
-    function (err, foundUser) {
-      if (err) {
-        console.log(err);
-      } else { 
-        if (foundUser) {
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
           res.render("secrets");
         }
       }
     }
-  );
+  });
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
 });
