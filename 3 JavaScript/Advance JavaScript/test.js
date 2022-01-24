4f378375503bfccd62d1212aded8ffe6180e0ffd
Nadeem <nadeemasgar18@gmail.com>
2022-01-24 15:36:40 +0530

    Practise

---
+++ 3 JavaScript/Advance JavaScript/test.js
@@ -0,0 +1,75 @@
+// var a23 = 1;
+// function b24() {
+//   a23 = 10;
+//   function a23() {
+//     return 11;
+//   }
+//   console.log(a23());
+// }
+// b24();
+// console.log(a23);
+
+// var a = 10;
+// function a() {
+//     console.log(20);
+// }
+
+// console.log(a());
+
+// var a = 10;
+// function b() {
+//      a = 20;
+//     console.log(a);
+// }
+
+// b();
+// console.log(a);
+
+// if(false) {
+//     console.log(10);
+// }
+// else {
+//     console.log(20);
+// }
+
+// var x = 10;
+// function test()
+// {
+// 	var x = 20;
+// 	console.log(x);
+// }
+
+// test();
+
+// var x = 10;
+
+// function test() {
+//   if (true) {
+//     var x = 50;
+//   }
+
+//   console.log(x);
+// }
+
+// test();
+
+
+
+// var x;
+// x = 10;
+
+// function test()
+// {
+// 	var x;
+// 	if (x > 20) {
+// 		x = 50;
+// 	}
+
+// 	console.log(x);
+// }
+
+// test();
+// console.log(x);
+
+var x = Math.floor("1000.01");
+console.log(typeof(x));
