2c8bca6c4db17bfef68acab2d25210b345a0137b
Nadeem <nadeemasgar18@gmail.com>
2022-02-03 00:14:40 +0530

    Form validation done

--- 9 Form/script.js
+++ 9 Form/script.js
@@ -12,16 +12,49 @@ function allFilled() {
 
 function validateform() {
   let name = document.myform.name.value;
+  let amount = document.myform.amount.value;
   let email = document.myform.email.value;
-  var regex = /^[a-zA-Z]+$/;
-  var isValid = regex.test(name);
-  if (name == "null" || name == "" || !isValid) {
+
+  /* Title Validation */
+  let name_regex = /^[a-zA-Z]+$/;
+  let isValidName = name_regex.test(name);
+  if (name == "null" || name == "" || !isValidName) {
     $("#user_title").css("border-color", "red");
     // alert("");
-    document.getElementById("usernameError").innerHTML =
+    document.getElementById("nameError").innerHTML =
       "Title can't be blank or format is wrong";
     return false;
   } else {
     $("#user_title").css("border-color", "");
+    document.getElementById("nameError").innerHTML = "";
+  }
+
+  /* Number Validation */
+  let regex_numbers = /^[0-9]+$/;
+  let isValidNumber = regex_numbers.test(amount);
+  if (amount == "null" || amount == "" || !isValidNumber) {
+    $("#user_amount").css("border-color", "red");
+    // alert("");
+    document.getElementById("amountError").innerHTML =
+      "Amount should only contain number ";
+    return false;
+  } else {
+    $("#user_amount").css("border-color", "");
+    document.getElementById("amountError").innerHTML = "";
+  }
+
+  /* Email Validation */
+  let regex_email = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+  let isValidEmail = regex_email.test(email);
+  if (email == "null" || email == "" || !isValidEmail) {
+    $("#user_email").css("border-color", "red");
+    // alert("");
+    document.getElementById("emailError").innerHTML = "Email format is wrong ";
+    return false;
+  } else {
+    $("#user_email").css("border-color", "");
+    document.getElementById("emailError").innerHTML = "";
   }
 }
